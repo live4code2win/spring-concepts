e97fc918c8c19718497acbfe772d7e0993e51b52
live4code2win <mrrat1601@gmail.com>
2020-12-30 02:42:09 +0530

    spring-concept project check-in

---
+++ src/test/java/com/spring/concepts/SpringConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
