97d1236142b6476a9d85c9b97d20ff2a987e5de6
live4code2win <mrrat1601@gmail.com>
2021-01-02 00:59:51 +0530

    spring xml based configuration...

--- src/main/java/com/spring/concepts/SpringConceptsApplication.java
+++ src/main/java/com/spring/concepts/SpringConceptsApplication.java
@@ -2,8 +2,10 @@ package com.spring.concepts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:spring.xml")
 public class SpringConceptsApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/spring/concepts/com/Employee.java
@@ -0,0 +1,103 @@
+package com.spring.concepts.com;
+
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private String code;
+	private String gender;
+	private double salary;
+	
+	private String email;
+	private long contact;
+	private String password;
+	
+	public Employee() {
+		System.out.println("loading the employee class...");
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public long getContact() {
+		return contact;
+	}
+	public void setContact(long contact) {
+		this.contact = contact;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + id;
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", code=" + code + ", gender=" + gender + ", salary=" + salary
+				+ ", email=" + email + ", contact=" + contact + ", password=" + password + "]";
+	}
+
+}
