d629c2baedbafb90703dc3626cbc3d13a32a9fff
live4code2win <mrrat1601@gmail.com>
2021-01-02 01:09:58 +0530

    two spring xml config files

--- src/main/java/com/spring/concepts/SpringConceptsApplication.java
+++ src/main/java/com/spring/concepts/SpringConceptsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:spring.xml")
+@ImportResource({"classpath:spring.xml", "classpath:spring1.xml"})
 public class SpringConceptsApplication {
 
 	public static void main(String[] args) {
